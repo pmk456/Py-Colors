017cbf35d82d185c0e59e951664b91b446a8cfe0
Pmk456 <45127373+pmk456@users.noreply.github.com>
2021-08-02 23:51:45 +0530

    Create __init__.py

---
+++ src/colors/__init__.py
@@ -0,0 +1,11 @@
+"""
+Author: Patan Musthakheem
+Licence: Apache 2.0
+Version: 0.1
+"""
+from __future__ import absolute_import
+from .version import *
+from .colors import *
+
+__author__ = "Patan Musthakheem"
+__licence__ = "Apache 2.0"
